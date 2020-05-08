f64226031ff85b425496965f7488683be7696ffa
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-05-08 16:51:05 -0500

    update logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, UPDATE_LOG, SET_CURRENT, CLEAR_CURRENT } from './types';
 
 // export const getLogs = () => {
 //     return async (dispatch) => {
@@ -61,10 +61,10 @@ export const addLog = (log) => async dispatch => {
 };
 
 // Delete Log from server
-export const deleteLog = (id) => async dispatch => {
+export const deleteLog = id => async dispatch => {
     try {
         setLoading();
-        
+
         await fetch(`/logs/${id}`, {
             method: 'DELETE'
         });
@@ -80,6 +80,49 @@ export const deleteLog = (id) => async dispatch => {
             payload: err.response.data
         });
     } 
+};
+
+// Update log on server
+export const updateLog = log => async dispatch => {
+    try {
+        setLoading();
+
+        const res = await fetch(`/logs/${log.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await res.json()
+        
+        dispatch({
+            type: UPDATE_LOG,
+            payload: data
+        });
+        
+    } catch (err) {
+        dispatch({ 
+            type: LOGS_ERROR, 
+            payload: err.response.data
+        });
+    } 
+};
+
+// Set current log
+export const setCurrent = log => {
+    return {
+        type: SET_CURRENT,
+        payload: log
+    }
+};
+
+// Clear current log
+export const clearCurrent = () => {
+    return {
+        type: CLEAR_CURRENT
+    }
 };
 
 //Set loading to true
